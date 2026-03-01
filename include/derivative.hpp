303df8311e9a852874731ba0261121f60cae08a8
Hadookhan <Hadikhan05@hotmail.co.uk>
2026-03-01 22:10:56 +0000

    Added infinite derivatives

--- include/derivative.hpp
+++ include/derivative.hpp
@@ -1,5 +1,6 @@
 #ifndef DERIVATIVE_HPP
 #define DERIVATIVE_HPP
+#include <cmath>
 
 namespace numerics {
     // Central difference derivative
@@ -14,6 +15,22 @@ namespace numerics {
     double second_derivative(Func f, double x) {
         double h = 1e-6;
         return (f(x + h) - 2*f(x) + f(x - h)) / (h*h);
+    }
+
+    // N-th derivative
+    template<typename Func>
+    double nth_derivative(Func f, double x, int n) {
+        if (n == 0)
+            return f(x);
+
+        if (n == 1)
+            return derivative(f, x);
+
+        auto prev = [&](double t) {
+            return nth_derivative(f, t, n - 1);
+        };
+
+        return derivative(prev, x);
     }
 }
 
--- src/math.cpp
+++ src/math.cpp
@@ -9,11 +9,12 @@ using namespace std;
 int main() {
 
     auto f = [](double x) {
-        return pow(sin(x),2);
+        return sin(x);
     };
 
     cout << derivative(f, 0) << "\n";
     cout << second_derivative(f, 0) << "\n";
+    cout << nth_derivative(f, 0, 7) << "\n";
 
     cout << linear_approx(f, 0.1) << "\n";
     cout << quadratic_approx(f, 0.1) << "\n";
